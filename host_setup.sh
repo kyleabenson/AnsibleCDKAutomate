4bfbc8dbc68ad725e4c98c5a89518f900a3eddfe
KYLE BENSON <kbenson@KYLEs-MBP.home>
2017-01-12 09:56:09 -0500

    Cleaned up host_setup script, switch from usage of which to type for more reliable checking

--- host_setup.sh
+++ host_setup.sh
@@ -14,28 +14,27 @@
 
   # Install Ansible
   if [[ $platform == 'darwin' ]]; then
-      if [ -f `which ansible` ]; then
+      if [[ -x $(type -p ansible) ]]; then
         echo "Both pip & Ansible are installed. Moving on.."
-      else
+      elif [[ ! -x $(type -p pip) ]]; then
         echo "Installing pip.."
         sudo easy_install pip
-        if ! [ -f `which ansible` ]; then
+      elif [[ ! -x $(type -p ansible) ]]; then
           echo "Installing Ansible..."
           sudo `which pip` install ansible
-          if [[ $? == 0 ]]; then
-            echo "Finished Ansible installation"
-          fi
-        fi
       fi
+      if [[ -x $(type -p ansible)  ]]; then
+          echo "Ansible ready for use"
+      fi	
   elif [[ $platform == 'linux' ]]; then
-      if [ -f `which ansible` ]; then
+      if [[ -x $(type -p ansible) ]]; then
         echo "Ansible is already installed."
-      elif [ ! -f `which ansible` ]; then
+      elif [[ ! -x $(type -p ansible) ]]; then
         echo "Enabling EPEL repo to install Ansible"
         sudo /usr/bin/rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
         sudo /usr/bin/yum install -y ansible
-        if [ $? == 0 ]; then
-          echo "Ansible successfully installed"
+        if [[ -x $(type -p ansible)  ]]; then
+          echo "Ansible ready for use"
         fi
         echo "Disabling EPEL repo"
         sudo rm -f /etc/yum.repos.d/epel*.repo
